9f771006195cc2317fa9c49f7f0b6c2e7ef277ec
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2022-04-13 21:22:05 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,52 @@
+var display = document.getElementById("displayText");
+var main = document.querySelector("main");
+
+function show1(){
+main.style.background ="linear-gradient(to left,red,orange) ";
+display.innerText = "linear-gradient(to left,red,orange) ";
+}
+function show2(){
+main.style.background ="linear-gradient(to top,yellow,orange,red,brown,black)  ";
+display.innerText = "linear-gradient(to top,yellow,orange,red,brown,black)  ";
+}
+function show3(){
+main.style.background ="radial-gradient(green,blue,indigo,black)  ";
+display.innerText = "radial-gradient(green,blue,indigo,black)  ";
+}
+function show4(){
+main.style.background ="radial-gradient(yellow,blue,white)  ";
+display.innerText = "radial-gradient(yellow,blue,white)  ";
+}
+function show5(){
+main.style.background ="linear-gradient(to left,red,purple,blue)  ";
+display.innerText = "linear-gradient(to left,red,purple,blue)  ";
+}
+function show6(){
+main.style.background ="linear-gradient(to top,yellow,pink)  ";
+display.innerText = "linear-gradient(to top,yellow,pink)  ";
+}
+function show7(){
+main.style.background ="linear-gradient(to bottom,purple,orange,green,white,black,red,blue,yellow)  ";
+display.innerText = "linear-gradient(to bottom,purple,orange,green,black,white,red,blue,yellow)  ";
+}
+function show8(){
+main.style.background ="linear-gradient(to bottom,tan,black)  ";
+display.innerText = "linear-gradient(to bottom,tan,black)  ";
+}
+function show9(){
+main.style.background ="linear-gradient(to left,brown,red,brown)  ";
+display.innerText = "linear-gradient(to left,brown,red,brown)  ";
+}
+function show10(){
+main.style.background ="radial-gradient(white,yellow,white,yellow)  ";
+display.innerText = "radial-gradient(white,yellow,white,yellow)  ";
+}
+function show11(){
+main.style.background ="linear-gradient(to left,green,yellow,green)  ";
+display.innerText = "linear-gradient(to left,green,yellow,green)  ";
+}
+function show12(){
+main.style.background ="linear-gradient(to top,blue,white)  ";
+display.innerText = "linear-gradient(to top,blue,white)  ";
+}
+
